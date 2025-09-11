efa6af728e18d3fb15476216925a9924622a8d16
Vivek2816 <vdoddi2@gitam.in>
2025-09-11 19:48:48 +0530

    Added alternative string compression logic using result list by iterative method

--- Python/string_compression.py
+++ Python/string_compression.py
@@ -1,29 +1,21 @@
-# hard-level
-
-# String compression with in-place modification typically means 
-# compressing a string by replacing consecutive repeated characters 
-# with the character followed by the count, 
-# all done directly in the original string's space (or an array of characters) without using extra space for another string.
-
-def String_compres(s):
-    chars=list(s)
-    n=len(chars)
-    i=0
-    new=0
+def string_compress(s):
+    if not s:
+        return ""
     
-    while i<n:
-        j=i
-        while j<n and chars[j]==chars[i]:
-            j+=1
-        chars[new]=chars[i]
-        new+=1
-        if j-i>1:
-            chars[new]=str(j-i)
-            new+=1
-        i=j
-    return "".join(chars[:new])
-    
-String=input("Enter String for compression: ")
-print(String_compres(String))
+    result = []
+    count = 1
 
-# try solving it iteratively
+    for i in range(1, len(s) + 1):
+        if i < len(s) and s[i] == s[i - 1]:
+            count += 1
+        else:
+            result.append(s[i - 1])
+            if count > 1:
+                result.append(str(count))
+            count = 1
+
+    return "".join(result)
+
+
+string = input("Enter String for compression: ")
+print(string_compress(string))
